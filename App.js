882102505ea29dbf8575f81e355c8bc143075b93
yurikbv <yurikbvv@gmail.com>
2020-08-11 00:15:34 +0300

    update

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const Home = () => (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your</Text>
       <StatusBar style="auto" />
     </View>
-  );
-}
+);
+
+const Detail = () => (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your</Text>
+    </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +29,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function StackNavigator() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{title: 'Awesome app'}}/>
+          <Stack.Screen name="Detail" component={Detail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  )
+}
+
+export default StackNavigator;
