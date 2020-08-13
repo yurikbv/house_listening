11f0d603d3cfc323f8d26cda68c3936a822ec017
yurikbv <yurikbvv@gmail.com>
2020-08-14 00:40:07 +0300

    finish

---
+++ Components/ListingDetail.ios.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from "styled-components/native";
+import { View, Text, Image, Dimensions } from "react-native";
+
+const ListingDetailWrapper = styled(View)`
+  display: flex;
+`;
+
+const Details = styled(View)`
+  padding: 5%;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background: rgba(0, 0, 255, .1);
+`;
+
+export const Title = styled(Text)`
+  flex-wrap: wrap;
+  width: 99%;
+  font-size: 30px;
+`;
+
+export const Price = styled(Text)`
+  font-weight: bold;
+  font-size: 20px;
+  color: blue;
+`;
+
+const Thumbnail = styled(Image)`
+  width: 100%;
+  height: ${Dimensions.get('window').height};
+`;
+
+const ListingDetail = ({item}) => {
+  return (
+      <ListingDetailWrapper>
+        <Thumbnail source={{uri: item.thumbnail}} />
+        <Details>
+          <Title>{item.title}</Title>
+          <Price>{item.price}</Price>
+        </Details>
+      </ListingDetailWrapper>
+  );
+};
+
+export default ListingDetail;
--- Screens/Detail.js
+++ Screens/Detail.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import ListingDetail from '../Components/ListingDetail';
 
 const Detail = ( {route} ) => {
 
   const {item} = route.params;
 
   return (
-      <View style={styles.container}>
-        <Text>{item.title}</Text>
+      <View>
+        <ListingDetail item={item}/>
       </View>)};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default Detail;
